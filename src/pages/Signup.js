fd1cd23cf7d0fc9cdf4dc9693fcbddf50ef3f69b
Michael Foley <foley.p.michael@gmail.com>
2022-04-29 11:34:50 +0100

    adjusted signup form to include first and last name.

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 export const Signup = () => {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [password, setPassword] = useState("");
 
     const navigate = useNavigate();
@@ -22,6 +24,12 @@ export const Signup = () => {
             <form onSubmit={handleSubmitForm}>
                 <label>Username</label>
                 <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+
+                <label>First Name</label>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+
+                <label>Last Name</label>
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
 
                 <label>Password</label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
