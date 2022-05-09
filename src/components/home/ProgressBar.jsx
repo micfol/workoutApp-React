10cd63f3373be9a84577bf5f1c67341173856e7e
Michael Foley <foley.p.michael@gmail.com>
2022-05-09 15:27:51 +0100

    Added route for getting total workout program progress for individual user. Added logic for progress bar to query database for count of total workouts.

--- src/components/home/ProgressBar.jsx
+++ src/components/home/ProgressBar.jsx
@@ -1,25 +1,46 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import { UserContext } from "../../context/user.context";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { progress } from "../../api";
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
   borderRadius: 5,
   [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
   },
   [`& .${linearProgressClasses.bar}`]: {
     borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+    backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
   },
 }));
 
 export default function CustomizedProgressBar() {
+  const value = useContext(UserContext);
+  const [goalProgress, setGoalProgress] = useState(0);
+
+  useEffect(() => {
+    const user = value ? value.user : null;
+    
+    const getProgress = async () => {
+      const response = await progress(user._id);
+      setGoalProgress(response.data);
+    };
+
+    getProgress();
+  }, [value]);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <p>Your Progress to Program Goal:</p>
-      <BorderLinearProgress variant="determinate" value={25} />
+      <BorderLinearProgress variant="determinate" value={goalProgress} />
     </Box>
   );
 }
